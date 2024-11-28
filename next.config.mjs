a27d249f2c015b4da414a3ba846fc31fd2ae0416
Sudhy <48507263+sudhy8@users.noreply.github.com>
2024-11-28 01:10:24 -0500

    setup

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    output: 'export',
+    basePath: '/sudhy8-portfolio',
+    assetPrefix: '/sudhy8-portfolio/',
+  };
+  
+
+  export default nextConfig;
